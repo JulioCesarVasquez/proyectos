8781be62f237b9dd9ef841b0ed67632eb3766103
Julio Cesar Vasquez <jv5353281@gmail.com>
2023-06-21 22:55:09 -0400

    codigo modificado

--- pra.js
+++ pra.js
@@ -1,22 +1,6 @@
-let pied="piedra";
-let tij="tijera";
-let papel="papel";
-function juegoPPT(per,cpu){
-    if(per==cpu){
-        console.log("Empate");
-    }else {
-        if(per=="piedra"&&cpu=="tijera" || per=="tijera"&&cpu=="papel" || per=="papel"&&cpu=="piedra"){
-            console.log("gana persona")
-                
-        }else{
-            console.log("gana cpu")
-        }
-
-    }    
+let a=parseInt(prompt("ingrese uun numero"));
+for(let i=1;i<=a;i++){
+    if(a%i===0){
+        console.log(i);
+    }
 }
-console.log(pied+"--"+tij)
-juegoPPT(pied,tij);
-console.log(tij+"--"+tij)
-juegoPPT(tij,tij)
-console.log(pied+"--"+papel)
-juegoPPT(pied,papel)
